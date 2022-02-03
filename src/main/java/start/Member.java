d95237a36717f9cd344e6ecf4f2323ded95e3063
ssooya90 <ssooya90@naver.com>
2022-02-03 09:01:08 +0900

    fix lombok 연결 및 세터 빌더 패턴 학습

--- src/main/java/start/Member.java
+++ src/main/java/start/Member.java
@@ -1,6 +1,25 @@
 package start;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="MEMBER")
+@Getter
+@Setter
 public class Member {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+
+
 
 
 
